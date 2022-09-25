d9c5adac61cb1191c93b10027d2d2663a9aa43b6
Gapur Kassym <gapur.kassym@gmail.com>
2022-09-25 23:31:08 +0300

    feat(src): using react-toastify

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import { ToastContainer, toast } from "react-toastify";
+
 import logo from './firebase-logo.png';
 
+const toastOptions = {
+  position: "top-right",
+  autoClose: 5000,
+  hideProgressBar: true,
+  closeOnClick: true,
+  pauseOnHover: true,
+};
+
 export default function App() {
+  const ToastifyNotification = () => (
+    <div className="push-notification">
+      <h2 className="push-notification-title">New Message</h2>
+      <p className="push-notification-text">You received a test push notification</p>
+    </div>
+  );
+
   return (
     <div className="app">
       <img src={logo} className="app-logo" alt="logo" />
+
+      <button
+        className="btn-primary"
+        onClick={() => toast(<ToastifyNotification />, toastOptions)}
+      >
+        Show notification
+      </button>
+
+      <ToastContainer />
     </div>
   );
 }
-
