3ffe06c148834f0913a0c17a9f745816f91c2296
Gapur Kassym <gapur.kassym@gmail.com>
2022-09-26 22:59:03 +0300

    feat(firebase): init firebase config

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
+
+console.log('*** Environment ***', process.env.REACT_APP_ENV)
+console.log('*** Firebase Config ***', firebaseConfig)
+
+const app = initializeApp(firebaseConfig);
