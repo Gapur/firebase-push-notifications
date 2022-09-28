5546b6bdd1d635bb190d1018e57662b824c0b187
Gapur Kassym <gapur.kassym@gmail.com>
2022-09-28 23:53:38 +0300

    feat(src): create firebase messaging service worker

---
+++ src/firebase-messaging-sw.js
@@ -0,0 +1,24 @@
+importScripts('https://www.gstatic.com/firebasejs/9.10.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.10.0/firebase-messaging-compat.js');
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('Received background message: ', payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = { body: payload.notification.body };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
